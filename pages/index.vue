db74d7363be3d6b192a9cf1cf0236294460d6bf6
伊藤拓哉 <itoutakuya@MacBook-Pro.local>
2020-09-17 23:20:39 +0900

    third commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,98 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        gravy-frontend
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="header">
+      <div class=image1>
+        <img src="IMG_1684.jpeg" width="210px" height="100px">
       </div>
     </div>
+    <el-row  :gutter="24">
+      <el-col :span="6">
+        <div class="aaa">
+          <div class="grid-content" style="padding: 10px; margin-bottom: 10px; border: 5px double #333333;">
+              検索窓
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content6">
+          <el-radio-group v-model="radio1">
+            <el-radio-button label="チャンネル"></el-radio-button>
+            <el-radio-button label="動画"></el-radio-button>
+          </el-radio-group>
+        </div>
+      </el-col>
+      <el-col :span="18">
+        <div class="grid-content2">
+          <el-radio-group v-model="radio2">
+            <el-radio-button label="カテゴリ1"></el-radio-button>
+            <el-radio-button label="カテゴリ2"></el-radio-button>
+            <el-radio-button label="カテゴリ3"></el-radio-button>
+            <el-radio-button label="カテゴリ4"></el-radio-button>
+            <el-radio-button label="カテゴリ5"></el-radio-button>
+            <el-radio-button label="カテゴリ6"></el-radio-button>
+            <el-radio-button label="カテゴリ7"></el-radio-button>
+            <el-radio-button label="カテゴリ8"></el-radio-button>
+            <el-radio-button label="カテゴリ9"></el-radio-button>
+            <el-radio-button label="カテゴリ10"></el-radio-button>
+            <el-radio-button label="カテゴリ11"></el-radio-button>
+            <el-radio-button label="カテゴリ12"></el-radio-button>
+            <el-radio-button label="カテゴリ13"></el-radio-button>
+            <el-radio-button label="カテゴリ14"></el-radio-button>
+          </el-radio-group>
+        </div>
+        <el-col :span="12">
+          <div class="bbb">
+          <div class="grid-content3" style="margin-bottom: 400px; border: 5px double #333333;">
+            高評価
+          </div>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="ccc">
+          <div class="grid-content4" style="margin-bottom: 400px; border: 5px double #333333;">
+            再生数
+          </div>
+          </div>
+        </el-col>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      radio1: 'チャンネル',
+      radio2: 'カテゴリ1'
+    }
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.header{
+  margin-top:10px;
+  filter: brightness(100%);
+  }
+.grid-content{
+  height:790px;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.ccc{
+  width:70pxpx;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.ddd{
+  width:70px;
 }
-
-.links {
-  padding-top: 15px;
+.grid-content2{
+  margin-top: 20px;
+}
+.grid-content3{
+  height:600px;
+  margin-top:50px;
+}
+.grid-content4{
+  height:600px;
+  margin-top:50px;
 }
 </style>
