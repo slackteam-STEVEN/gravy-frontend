1072cfafc5b736e8902ce6c56aa1a58ca9b7909e
伊藤拓哉 <itoutakuya@MacBook-Pro.local>
2020-09-18 00:53:10 +0900

    fours commit

--- pages/top_movie.vue
+++ pages/top_movie.vue
@@ -9,7 +9,7 @@
       <el-col :span="6">
         <div class="aaa">
           <div class="grid-content" style="padding: 10px; margin-bottom: 10px; border: 5px double #333333;">
-              検索窓
+            検索窓
           </div>
         </div>
       </el-col>
@@ -26,10 +26,28 @@
         </div>
       </el-col>
       <el-col :span="18">
+        <div class="grid-content2">
+          <el-radio-group v-model="radio2">
+            <el-radio-button label="カテゴリ1"></el-radio-button>
+            <el-radio-button label="カテゴリ2"></el-radio-button>
+            <el-radio-button label="カテゴリ3"></el-radio-button>
+            <el-radio-button label="カテゴリ4"></el-radio-button>
+            <el-radio-button label="カテゴリ5"></el-radio-button>
+            <el-radio-button label="カテゴリ6"></el-radio-button>
+            <el-radio-button label="カテゴリ7"></el-radio-button>
+            <el-radio-button label="カテゴリ8"></el-radio-button>
+            <el-radio-button label="カテゴリ9"></el-radio-button>
+            <el-radio-button label="カテゴリ10"></el-radio-button>
+            <el-radio-button label="カテゴリ11"></el-radio-button>
+            <el-radio-button label="カテゴリ12"></el-radio-button>
+            <el-radio-button label="カテゴリ13"></el-radio-button>
+            <el-radio-button label="カテゴリ14"></el-radio-button>
+          </el-radio-group>
+        </div>
         <el-col :span="12">
           <div class="bbb">
           <div class="grid-content3" style="margin-bottom: 400px; border: 5px double #333333;">
-            チャンネル登録者数
+            高評価
           </div>
           </div>
         </el-col>
@@ -49,7 +67,8 @@
 export default {
   data () {
     return {
-      radio1: 'チャンネル'
+      radio1: '動画',
+      radio2: 'カテゴリ1'
     }
   }
 }
@@ -71,13 +90,17 @@ export default {
 }
 .grid-content2{
   margin-top: 20px;
+  margin-right: 20px;
 }
 .grid-content3{
-  height:700px;
+  height:600px;
   margin-top:50px;
 }
 .grid-content4{
-  height:700px;
+  height:600px;
   margin-top:50px;
+}
+.el-radio-group1{
+  width:100px;
 }
 </style>
